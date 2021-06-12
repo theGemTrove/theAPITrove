d48c5e0a298e898cb36ca25ddb34ee5063b3b2b1
Dustin K Greco <dkgreco.dev@gmail.com>
2021-06-12 14:05:59 -0700

    Initial Commit Of TGT API Library

---
+++ readMe.md
@@ -0,0 +1,22 @@
+# The GemTrove API Library
+> Backend API Cookbook
+
+## Usage
+> Visit the following link: 
+
+## Install Dependencies
+```
+yarn install
+```
+
+## Run Application
+```
+# Development Mode
+yarn dev
+
+# Production Mode
+yarn start
+```
+
+- Version: 1.0.0
+- License: MIT
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const devMode = env => env === 'development';
+if (devMode) {
+	require('dotenv').config({ path: './config/config.env' });
+}
+const
+	express = require('express'),
+	handler = () => {},
+	PORT		= process.env.PORT || 5000,
+	app 		= express();
+
+app.use(express.static('public'));
+
+app.get(
+	'/',
+	(req, res, next) => {
+
+	}
+);
+
+app.listen(PORT, () => {
+	if (devMode) {
+		const debug = require('debug')('server.js');
+		debug(`Server is up and running on port: ${PORT}`);
+	}
+});
